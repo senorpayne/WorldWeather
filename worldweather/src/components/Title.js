d0c5a5575b62d4c95a4420172d3f66dc54fcc35c
Jason Payne <senorpayne@yahoo.com>
2018-05-30 11:09:45 -0500

    first

---
+++ worldweather/src/components/Title.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Titles extends React.Component{
+
+	render(){
+
+		return(
+
+			<div>
+
+			<h1>Weather Finder</h1>
+			<p>Find out weather conditions around the world!</p>
+
+			</div>
+			);
+
+
+	}
+};
+
+export default Titles;
