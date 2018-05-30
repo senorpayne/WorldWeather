67be249e1fc677004aa32d1dec3a5d8ac2901d65
Jason Payne <senorpayne@yahoo.com>
2018-05-30 15:10:26 -0500

    https

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ e.preventDefault();
 const city = e.target.elements.city.value;
 const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${key}&units=imperial
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${key}&units=imperial
 `);
 
     const data = await api_call.json();
